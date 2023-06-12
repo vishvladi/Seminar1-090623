28c615e1d881b79462cd964a2235d2ded7664bfb
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2023-06-12 22:13:09 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//  на вход 2 числа, на выход большее и меньшее
+Console.Write("Введите первое число: ");
+string inputedString = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString);
+Console.WriteLine($"Вы ввели число {number1}");
+Console.Write("Введите второе число: ");
+inputedString = Console.ReadLine();
+int number2 = Convert.ToInt32(inputedString);
+Console.WriteLine($"Вы ввели число {number2}");
+
+if (number1 > number2){
+     
+    Console.WriteLine($"Наибольшее число {number1}, а наименьшее число {number2}");
+}
+else
+{
+   Console.WriteLine($"Наибольшее число {number2}, а наименьшее число {number1}"); 
+}
+
