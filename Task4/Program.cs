8da63568173f786746834281ffd59e4f6bd6b0bb
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2023-06-13 19:26:48 +0300

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход число N, а на выходе все четные числа от 1 до N.
+Console.Write("Введите число > 1 ");
+string inputedString = Console.ReadLine();
+int N = Convert.ToInt32(inputedString);
+Console.WriteLine($"Вы ввели число {N}");
+Console.WriteLine($"Все четные числа от 1 до {N}:");
+int count = 1;
+while (count <= N){
+    if(count % 2 == 0){
+    Console.Write(count + " ");
+    }
+    count ++;
+}
+    
+
